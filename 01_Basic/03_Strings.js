040807d41ea1df5c89b0daf1dd380f6acdb5dba8
ASR-4 <lootmoneyyt@gmail.com>
2025-06-30 18:50:51 +0000

    strings method in js

---
+++ 01_Basic/03_Strings.js
@@ -0,0 +1,24 @@
+const name = "Sumant Rai"
+
+console.log(name.length);
+console.log(name.charAt(2));
+
+console.log(name.toLocaleUpperCase());
+console.log(name.toLocaleLowerCase());
+
+console.log(name.endsWith("i"));
+console.log(name.indexOf("t"));
+console.log(name.concat(" Apeksha More"));
+console.log(name.replace("Sumant Rai","Apeksha More"));
+console.log(name.search("i"));
+
+
+
+console.log(name.trim(" "));
+
+
+
+
+
+
+
