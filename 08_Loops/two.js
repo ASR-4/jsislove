448ddcbf304cddcfda959482f3c04b8fb84e3ba0
ASR-4 <lootmoneyyt@gmail.com>
2025-07-02 11:09:16 +0000

    while and do while loop in js

---
+++ 08_Loops/two.js
@@ -0,0 +1,32 @@
+// while(condtion){
+
+// i++
+// }
+
+// let i = 1
+// while (i <= 10) {
+
+//     console.log(`Value of i is ${i}`);
+//     i++
+// }
+
+const myFriends = ["Shubhangi", "Mandal", "Aakash", "Rishu"]
+let i = 0
+while (i < myFriends.length) {
+    // console.log(`My Friends is ${myFriends[i]}`);
+    i++  
+}
+
+
+
+// do {
+    
+// } while (condition);
+
+let score = 11
+
+do {
+   console.log(`score is ${score}`);
+   score++
+    
+} while (score <= 10);
